23ab40fc5876bef26ddcf0072d7e8fa49b5e2a27
Nullptr <tsng.nullptr@outlook.com>
2022-01-18 20:21:34 +0800

    Timeout & Thread lock

--- server/take_picture.cpp
+++ server/take_picture.cpp
@@ -1,5 +1,7 @@
 #include <mutex>
+#include <atomic>
 #include <cerrno>
+#include <thread>
 #include <cstring>
 #include <fcntl.h>
 #include <unistd.h>
@@ -11,7 +13,17 @@
 namespace OHOS::HITCamera {
 
     namespace {
-        std::mutex mtx;
+        int camera = -1;
+        v4l2_buffer vbuf;
+        void* buffer;
+        std::mutex mtx; // Client invocation lock
+        uint32_t widthCached, heightCached;
+
+        std::mutex watchDogMtx;
+        std::atomic_bool watchDogCancel = true;
+        std::atomic<time_t> timeStamp;
+
+        void WatchDogImpl();
 
         int xioctl(int fd, int request, void* arg) {
             int r;
@@ -21,118 +33,166 @@ namespace OHOS::HITCamera {
             } while (-1 == r && EINTR == errno);
             return r;
         }
+
+        int StartStream() { // Synchronized by Capture() with mtx
+            if (camera != -1) return FAILED_OCCUPIED;
+
+            camera = open("/dev/video0", O_RDWR);
+            if (camera == -1) {
+                LOGE("Failed opening video device: %s", strerror(errno));
+                return FAILED_OPEN_DEVICE;
+            }
+
+            v4l2_capability caps{};
+            if (-1 == xioctl(camera, VIDIOC_QUERYCAP, &caps)) {
+                LOGE("Failed querying capabilities: %s", strerror(errno));
+                return FAILED_QUERY_CAPABILITIES;
+            }
+
+            // Set format
+            v4l2_format fmt{};
+            fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            fmt.fmt.pix.width = widthCached;
+            fmt.fmt.pix.height = heightCached;
+            fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+            fmt.fmt.pix.field = V4L2_FIELD_NONE;
+            if (-1 == xioctl(camera, VIDIOC_S_FMT, &fmt)) {
+                LOGE("Failed setting pixel format: %s", strerror(errno));
+                return FAILED_SET_FORMAT;
+            }
+
+            // Request buffer
+            v4l2_requestbuffers req{};
+            req.count = 1;
+            req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            req.memory = V4L2_MEMORY_MMAP;
+            if (-1 == xioctl(camera, VIDIOC_REQBUFS, &req)) {
+                LOGE("Failed requesting buffer: %s", strerror(errno));
+                return FAILED_REQUEST_BUFFER;
+            }
+
+            // Query buffer
+            vbuf = v4l2_buffer();
+            vbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            vbuf.memory = V4L2_MEMORY_MMAP;
+            vbuf.index = 0;
+            if (-1 == xioctl(camera, VIDIOC_QUERYBUF, &vbuf)) {
+                LOGE("Failed querying buffer: %s", strerror(errno));
+                return FAILED_QUERY_BUFFER;
+            }
+            buffer = mmap(nullptr, vbuf.length, PROT_READ | PROT_WRITE, MAP_SHARED, camera, vbuf.m.offset);
+            LOGD("Query buffer: Lenght: %d Address: %p", vbuf.length, buffer);
+
+            if (-1 == xioctl(camera, VIDIOC_STREAMON, &vbuf.type)) {
+                LOGE("Failed starting capture: %s", strerror(errno));
+                munmap(buffer, vbuf.length);
+                return FAILED_START_STREAM;
+            }
+
+            return NO_ERROR;
+        }
+
+        void StopStream() {// Synchronized by WatchDog with watchDogMtx
+            munmap(buffer, vbuf.length);
+            if (-1 == xioctl(camera, VIDIOC_STREAMOFF, &vbuf.type)) {
+                LOGE("Failed when stream off: %s", strerror(errno));
+            }
+            close(camera);
+            camera = -1;
+        }
+
+        void WatchDogImpl() {
+            watchDogMtx.lock();
+            while (true) {
+                if (watchDogCancel) break;
+                if (time(nullptr) - 5 > timeStamp) break;
+                std::this_thread::sleep_for(std::chrono::microseconds(100));
+            }
+            /*
+             * Prevent such situation:
+             * ClientThread    WatchDogThread
+             *      X          StoppingStream
+             *    Enter              |
+             *      |                X
+             *   Capture <-- Boom!
+             */
+            bool lockClientMtx = mtx.try_lock();
+            StopStream();
+            watchDogMtx.unlock();
+            if (lockClientMtx) mtx.unlock();
+        }
+
+        void CancelStream() {
+            watchDogCancel = true;
+            watchDogMtx.lock();
+            watchDogMtx.unlock();
+            watchDogCancel = false;
+        }
     }
 
-#define FAIL(reason) close(fd); mtx.unlock(); return reason
+#define FAIL(reason) mtx.unlock(); return reason
 #define CHECK_MAP(func) if (!(func)) {           \
             LOGE("%s", "Failed mapping memory"); \
             FAIL(FAILED_MAP);                    \
         }
-#define UNMAP() munmap(buffer, buf.length)
 
     std::variant<sptr<Ashmem>, int> TakeOnePicture(uint32_t width, uint32_t height) {
+        timeStamp = std::numeric_limits<time_t>::max(); // Prevent WatchDog invoke `StopStream` when taking picture
         mtx.lock();
-        int fd = open("/dev/video0", O_RDWR);
-        if (fd == -1) {
-            LOGE("Failed opening video device: %s", strerror(errno));
-            FAIL(FAILED_OPEN_DEVICE);
+
+        if (width != widthCached || height != heightCached) { // Pixel not corresponding, cancel stream
+            widthCached = width;
+            heightCached = height;
+            CancelStream();
         }
 
-        v4l2_capability caps{};
-        if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &caps)) {
-            LOGE("Failed querying capabilities: %s", strerror(errno));
-            FAIL(FAILED_QUERY_CAPABILITIES);
+        if (watchDogMtx.try_lock()) { // Stream off, make start
+            watchDogMtx.unlock();
+            int error = StartStream();
+            if (error) {
+                StopStream();
+                FAIL(error);
+            }
+            std::thread(WatchDogImpl).detach();
         }
 
-        // Set format
-        v4l2_format fmt{};
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        fmt.fmt.pix.width = width;
-        fmt.fmt.pix.height = height;
-        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
-        fmt.fmt.pix.field = V4L2_FIELD_NONE;
-        if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt)) {
-            LOGE("Failed setting pixel format: %s", strerror(errno));
-            FAIL(FAILED_SET_FORMAT);
-        }
-
-        // Request buffer
-        v4l2_requestbuffers req{};
-        req.count = 1;
-        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        req.memory = V4L2_MEMORY_MMAP;
-        if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req)) {
-            LOGE("Failed requesting buffer: %s", strerror(errno));
-            FAIL(FAILED_REQUEST_BUFFER);
-        }
-
-        // Query buffer
-        v4l2_buffer buf{};
-        void* buffer;
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        buf.index = 0;
-        if (-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf)) {
-            LOGE("Failed querying buffer: %s", strerror(errno));
-            FAIL(FAILED_QUERY_BUFFER);
-        }
-        buffer = mmap(nullptr, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
-        LOGD("Query buffer: Lenght: %d Address: %p", buf.length, buffer);
-
-        memset(&buf, 0, sizeof(buf));
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        buf.index = 0;
-
-        if (-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type)) {
-            LOGE("Failed starting capture: %s", strerror(errno));
-            UNMAP();
-            FAIL(FAILED_START_CAPTURE);
-        }
-
-        if (-1 == ioctl(fd, VIDIOC_QBUF, &buf)) {
+        if (-1 == ioctl(camera, VIDIOC_QBUF, &vbuf)) {
             LOGE("Failed queueing buffer: %s", strerror(errno));
-            UNMAP();
             FAIL(FAILED_QUEUE_BUFFER);
         }
 
         fd_set fds;
         FD_ZERO(&fds);
-        FD_SET(fd, &fds);
+        FD_SET(camera, &fds);
         timeval tv{};
         tv.tv_sec = 2;
 
         LOGD("%s", "Waiting for frame");
-        if (-1 == select(fd + 1, &fds, nullptr, nullptr, &tv)) {
+        if (-1 == select(camera + 1, &fds, nullptr, nullptr, &tv)) {
             LOGE("Failed waiting for frame: %s", strerror(errno));
-            UNMAP();
+            CancelStream();
             FAIL(FAILED_WAIT_FRAME);
         }
 
         LOGD("%s", "Retrieving frame");
-        if (-1 == xioctl(fd, VIDIOC_DQBUF, &buf)) {
+        if (-1 == xioctl(camera, VIDIOC_DQBUF, &vbuf)) {
             LOGE("Failed retrieving frame: %s", strerror(errno));
-            UNMAP();
+            CancelStream();
             FAIL(FAILED_RETRIEVE_FRAME);
         }
 
         LOGD("%s", "Writing image");
-        auto size = static_cast<int32_t>(buf.bytesused);
+        auto size = static_cast<int32_t>(vbuf.bytesused);
         auto ashmem = Ashmem::CreateAshmem("hit_picture", size);
         CHECK_MAP(ashmem != nullptr)
         CHECK_MAP(ashmem->MapReadAndWriteAshmem())
         CHECK_MAP(ashmem->WriteToAshmem(buffer, size, 0))
-        UNMAP();
 
-        if (-1 == xioctl(fd, VIDIOC_STREAMOFF, &buf.type)) {
-            LOGE("Failed when stream off: %s", strerror(errno));
-            ashmem->UnmapAshmem();
-            ashmem->CloseAshmem();
-            FAIL(FAILED_STREAM_OFF);
-        }
-
-        close(fd);
+        timeStamp = time(nullptr); // Set timeStamp to now
         mtx.unlock();
         return ashmem;
     }
+
+#undef FAIL
+#undef CHECK_MAP
 }
