1a462471776e8c1b9c9a1ed9137a096daddbc541
Nullptr <52071314+Dr-TSNG@users.noreply.github.com>
2022-03-28 19:37:43 +0800

    More test

--- test/native_test.cpp
+++ test/native_test.cpp
@@ -19,6 +19,132 @@ int xioctl(int fd, int request, void* arg) {
     return r;
 }
 
+bool try_mmap(int camera) {
+    LOGD("Trying mmap");
+    // Request buffer
+    v4l2_requestbuffers req{};
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+    if (-1 == xioctl(camera, VIDIOC_REQBUFS, &req)) {
+        LOGE("Failed requesting buffer: %s", strerror(errno));
+        return false;
+    }
+
+    // Query buffer
+    v4l2_buffer vbuf{};
+    void* buffer;
+    vbuf = v4l2_buffer();
+    vbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    vbuf.memory = V4L2_MEMORY_MMAP;
+    vbuf.index = 0;
+    if (-1 == xioctl(camera, VIDIOC_QUERYBUF, &vbuf)) {
+        LOGE("Failed querying buffer: %s", strerror(errno));
+        return false;
+    }
+    buffer = mmap(nullptr, vbuf.length, PROT_READ | PROT_WRITE, MAP_SHARED, camera, vbuf.m.offset);
+    LOGD("Query buffer: Lenght: %d Address: %p", vbuf.length, buffer);
+
+    if (-1 == xioctl(camera, VIDIOC_STREAMON, &vbuf.type)) {
+        LOGE("Failed starting capture: %s", strerror(errno));
+        munmap(buffer, vbuf.length);
+        return false;
+    }
+
+    if (-1 == ioctl(camera, VIDIOC_QBUF, &vbuf)) {
+        LOGE("Failed queueing buffer: %s", strerror(errno));
+        return false;
+    }
+
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(camera, &fds);
+    timeval tv{};
+    tv.tv_sec = 2;
+
+    LOGD("Waiting for frame");
+    if (-1 == select(camera + 1, &fds, nullptr, nullptr, &tv)) {
+        LOGE("Failed waiting for frame: %s", strerror(errno));
+        return false;
+    }
+
+    LOGD("Retrieving frame");
+    if (-1 == xioctl(camera, VIDIOC_DQBUF, &vbuf)) {
+        LOGE("Failed retrieving frame: %s", strerror(errno));
+        return false;
+    }
+
+    LOGD("Stopping stream");
+    munmap(buffer, vbuf.length);
+    if (-1 == xioctl(camera, VIDIOC_STREAMOFF, &vbuf.type)) {
+        LOGE("Failed when stream off: %s", strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
+bool try_user_pointer(int camera) {
+    LOGD("Trying user pointer");
+    // Request buffer
+    v4l2_requestbuffers req{};
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_USERPTR;
+    if (-1 == xioctl(camera, VIDIOC_REQBUFS, &req)) {
+        LOGE("Failed requesting buffer: %s", strerror(errno));
+        return false;
+    }
+
+    // Query buffer
+    v4l2_buffer vbuf{};
+    void* buffer;
+    vbuf = v4l2_buffer();
+    vbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    vbuf.memory = V4L2_MEMORY_USERPTR;
+    vbuf.index = 0;
+
+    if (-1 == xioctl(camera, VIDIOC_STREAMON, &vbuf.type)) {
+        LOGE("Failed starting capture: %s", strerror(errno));
+        return false;
+    }
+
+    if (-1 == ioctl(camera, VIDIOC_QBUF, &vbuf)) {
+        LOGE("Failed queueing buffer: %s", strerror(errno));
+        return false;
+    }
+
+    buffer = reinterpret_cast<void*>(vbuf.m.userptr);
+    LOGD("Query buffer: Lenght: %d Address: %p", vbuf.length, buffer);
+
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(camera, &fds);
+    timeval tv{};
+    tv.tv_sec = 2;
+
+    LOGD("Waiting for frame");
+    if (-1 == select(camera + 1, &fds, nullptr, nullptr, &tv)) {
+        LOGE("Failed waiting for frame: %s", strerror(errno));
+        return false;
+    }
+
+    LOGD("Retrieving frame");
+    if (-1 == xioctl(camera, VIDIOC_DQBUF, &vbuf)) {
+        LOGE("Failed retrieving frame: %s", strerror(errno));
+        return false;
+    }
+
+    LOGD("Stopping stream");
+    munmap(buffer, vbuf.length);
+    if (-1 == xioctl(camera, VIDIOC_STREAMOFF, &vbuf.type)) {
+        LOGE("Failed when stream off: %s", strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
 int main() {
     auto camera = open("/dev/video0", O_RDWR);
 
@@ -79,64 +205,17 @@ int main() {
         return 0;
     }
 
-    // Request buffer
-    v4l2_requestbuffers req{};
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
-    if (-1 == xioctl(camera, VIDIOC_REQBUFS, &req)) {
-        LOGE("Failed requesting buffer: %s", strerror(errno));
-        return 0;
+    if (try_mmap(camera)) {
+        LOGD("Mmap succeed!!!");
+    } else {
+        LOGD("Mmap failed");
+    }
+    if (try_user_pointer(camera)) {
+        LOGD("User pointer succeed!!!");
+    } else {
+        LOGD("User pointer failed");
     }
 
-    // Query buffer
-    v4l2_buffer vbuf{};
-    void* buffer;
-    vbuf = v4l2_buffer();
-    vbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    vbuf.memory = V4L2_MEMORY_MMAP;
-    vbuf.index = 0;
-    if (-1 == xioctl(camera, VIDIOC_QUERYBUF, &vbuf)) {
-        LOGE("Failed querying buffer: %s", strerror(errno));
-        return 0;
-    }
-    buffer = mmap(nullptr, vbuf.length, PROT_READ | PROT_WRITE, MAP_SHARED, camera, vbuf.m.offset);
-    LOGD("Query buffer: Lenght: %d Address: %p", vbuf.length, buffer);
-
-    if (-1 == xioctl(camera, VIDIOC_STREAMON, &vbuf.type)) {
-        LOGE("Failed starting capture: %s", strerror(errno));
-        munmap(buffer, vbuf.length);
-        return 0;
-    }
-
-    if (-1 == ioctl(camera, VIDIOC_QBUF, &vbuf)) {
-        LOGE("Failed queueing buffer: %s", strerror(errno));
-        return 0;
-    }
-
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(camera, &fds);
-    timeval tv{};
-    tv.tv_sec = 2;
-
-    LOGD("Waiting for frame");
-    if (-1 == select(camera + 1, &fds, nullptr, nullptr, &tv)) {
-        LOGE("Failed waiting for frame: %s", strerror(errno));
-        return 0;
-    }
-
-    LOGD("Retrieving frame");
-    if (-1 == xioctl(camera, VIDIOC_DQBUF, &vbuf)) {
-        LOGE("Failed retrieving frame: %s", strerror(errno));
-        return 0;
-    }
-
-    LOGD("Stopping stream");
-    munmap(buffer, vbuf.length);
-    if (-1 == xioctl(camera, VIDIOC_STREAMOFF, &vbuf.type)) {
-        LOGE("Failed when stream off: %s", strerror(errno));
-    }
     close(camera);
 
     return 0;
